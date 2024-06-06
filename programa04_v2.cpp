d7809d4021c5a04bad49ab1499805902cfb1fd23
cibelevs <cibelevale.estudante@gmail.com>
2024-06-06 08:43:25 -0300

    Update programa04_v2.cpp

--- programa04_v2.cpp
+++ programa04_v2.cpp
@@ -1,6 +1,6 @@
 #include<stdio.h>
   const int MAX = 6;
-  int F[MAX];
+  int x;
   int i;
   
 int main ()
@@ -9,12 +9,13 @@ int main ()
   for(i=0; i<MAX; i++)
   {
     if(i <= 1)
-        F[i] = i;
+        x = i;
     else 
     {
-        F[i] = F[i - 1] + F[i - 2];
+        x = x + i - 2;
+      
     }
-    printf(" %d ", F[i]);
+    printf(" %d ", x);
   }
   
     return 0;
